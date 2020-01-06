0b4e8ad2ae114855c1a53f1da4cbb8a5b666c05a
Anneloes <anneloes9405@hotmail.com>
2020-01-06 20:47:06 +0000

    Created own assertEqual with four test scenarios

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😄💚 Assertion passed: " + actual + " === " + expected);
+  } else {
+    console.log("😱⛔ Assertion failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("Lighthouse", "Lighthouse");
+assertEqual(3, 2);
