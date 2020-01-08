2c1a2216d9de74a1f9dbe73d6dd986697a39c8b3
Anneloes <anneloes9405@hotmail.com>
2020-01-08 19:25:22 +0000

    function that counts numbers in a sentence, puts those into object

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😄💚 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😱⛔ Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+// returns a count of each letter in sentence 
+//  into an object
+// skip/not count spaces
+const countLetters = function (sentence) {
+  let totalLetters = {}
+  for (letter of sentence) {
+    if (letter !== " ") {
+      if (totalLetters[letter]) {
+        totalLetters[letter] += 1;
+      } else {
+        totalLetters[letter] = 1
+      }
+    }
+  }
+  return totalLetters;
+}
+
+// TEST CASE
+// assertEqual(countLetters("lighthouse in the house"), { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1,})
+
+// STRETCH: iterate over elements of actual and 
+// expected object, see if they match up (instead of 
+// comparing results with own eyes).
