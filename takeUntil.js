ade676df912586e3bf593b9e2c26cc3b62a8ea5a
Anneloes <anneloes9405@hotmail.com>
2020-01-09 20:51:01 +0000

    added takeUntil, a function that loops through and prints elements of array until certain condition is met

---
+++ takeUntil.js
@@ -0,0 +1,24 @@
+const takeUntil = function(array, callback) {
+  let newArray = [];
+  //array.forEach(function(element){
+  for (let element of array) {
+    if (callback(element)) {
+      return newArray;
+    } newArray.push(element);
+  }
+};
+
+// TEST CASES
+
+// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+// const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
+
+// console.log('---');
+
+// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
+
+// assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+// assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
